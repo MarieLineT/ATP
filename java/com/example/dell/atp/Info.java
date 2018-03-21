d0b7aa7282d8526d95bbedf5b48c44ebd7b06937
MarieLineT <mtemps@ensc.fr>
2018-03-21 16:19:27 +0100

    fonction enregistrerDonnee

--- java/com/example/dell/atp/Info.java
+++ java/com/example/dell/atp/Info.java
@@ -10,8 +10,13 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static com.example.dell.atp.Sign.getUid;
 
 public class Info extends AppCompatActivity {
 
@@ -54,8 +59,8 @@ public class Info extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Enregistrer informations utilisateurs
-                String nom = mNom.getText().toString().trim();
-                String prenom = mPrenom.getText().toString().trim();
+                String nouveauNom = mNom.getText().toString().trim();
+                String nouveauPrenom = mPrenom.getText().toString().trim();
                 int age = mAge.getInputType();
 
                 //Récupérer actvité chosie par utilisateur
@@ -63,13 +68,22 @@ public class Info extends AppCompatActivity {
                 if (mActivite.getSelectedItem() == "Professionnel") activite = "Professionnel";
                 if (mActivite.getSelectedItem() == "Particulier") activite = "Particulier";
 
+                //Ajouter les valeurs à la BD
                 //Certains champs peuvent ne pas être remplis : comment le prendre en compte ?
-
+                //Peut pas créer une fonction car certains paramètres peuvent changer : cas par cas
+                if(nouveauNom != null){
+                    enregistrerDonnee("nom", nouveauNom );
+                }
+                else if(nouveauPrenom != null){
+                    enregistrerDonnee("prenom", nouveauPrenom);
+                }
             }
         });
+    }
 
-
-
-
+    //Fonction permettant de modifier une valeur donnée
+    public void enregistrerDonnee(String donnee, String valeur){
+        String UserId = getUid();
+        mDatabase.child("users").child(UserId).child(donnee).setValue(valeur);
     }
 }
