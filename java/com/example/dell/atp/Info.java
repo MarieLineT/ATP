81626d8d0de45199a3da99ce1415c392edb15e3d
MarieLineT <mtemps@ensc.fr>
2018-03-24 11:08:31 +0100

    OnPressedBack()

--- java/com/example/dell/atp/Info.java
+++ java/com/example/dell/atp/Info.java
@@ -1,5 +1,6 @@
 package com.example.dell.atp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,6 +31,18 @@ public class Info extends AppCompatActivity {
     //Déclaration DB
     DatabaseReference mDatabase;
 
+    private static long back_pressed;
+    @Override
+    public void onBackPressed(){
+        if (back_pressed + 2000 > System.currentTimeMillis()){
+            super.onBackPressed();
+        }
+        else{
+            Toast.makeText(getBaseContext(), "Appuyez de nouveau pour sortir", Toast.LENGTH_SHORT).show();
+            back_pressed = System.currentTimeMillis();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +52,10 @@ public class Info extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //Référencement des éléments graphiques
-        mNom =  findViewById(R.id.nom);
-        mPrenom =  findViewById(R.id.prenom);
-        mAge =  findViewById(R.id.age);
-        mProgressBar =  findViewById(R.id.progressBar);
+        mNom = findViewById(R.id.nom);
+        mPrenom = findViewById(R.id.prenom);
+        mAge = findViewById(R.id.age);
+        mProgressBar = findViewById(R.id.progressBar);
         mBtnModif = findViewById(R.id.btn_modif);
         mActivite = findViewById(R.id.activite);
         mPro = findViewById(R.id.pro);
@@ -52,37 +66,51 @@ public class Info extends AppCompatActivity {
         mActivite.setAdapter(adapter);
         //Afficher renseignement profession
         //Pas sûre de pouvoir le mettre là
-        if(mActivite.getSelectedItem() == "Professionnel") mPro.setVisibility(View.VISIBLE);
+        if (mActivite.getSelectedItem() == "Professionnel") mPro.setVisibility(View.VISIBLE);
 
 
         mBtnModif.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mProgressBar.setVisibility(View.VISIBLE);
+
                 //Enregistrer informations utilisateurs
                 String nouveauNom = mNom.getText().toString().trim();
                 String nouveauPrenom = mPrenom.getText().toString().trim();
-                int age = mAge.getInputType();
+                String nouvelleActivite = "";
 
                 //Récupérer actvité chosie par utilisateur
-                String activite ="";
-                if (mActivite.getSelectedItem() == "Professionnel") activite = "Professionnel";
-                if (mActivite.getSelectedItem() == "Particulier") activite = "Particulier";
+                if (mActivite.getSelectedItem() == "Professionnel")
+                    nouvelleActivite = "Professionnel(le)";
+                else if (mActivite.getSelectedItem() == "Particulier")
+                    nouvelleActivite = "Particulier";
 
                 //Ajouter les valeurs à la BD
                 //Certains champs peuvent ne pas être remplis : comment le prendre en compte ?
-                //Peut pas créer une fonction car certains paramètres peuvent changer : cas par cas
-                if(nouveauNom != null){
-                    enregistrerDonnee("nom", nouveauNom );
+                //Peut pas créer une seule fonction : cas par cas
+                if (!nouveauNom.isEmpty()) {
+                    enregistrerDonnee("_nom", nouveauNom);
                 }
-                else if(nouveauPrenom != null){
-                    enregistrerDonnee("prenom", nouveauPrenom);
+                if (!nouveauPrenom.isEmpty()) {
+                    enregistrerDonnee("_prenom", nouveauPrenom);
                 }
+                if (!nouvelleActivite.isEmpty()) {
+                    enregistrerDonnee("_activite", nouvelleActivite);
+                }
+
+
+                //Aller à l'activité Account
+                Toast.makeText(Info.this, "Modifications enregistrées !", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Info.this, Account.class));
+                finish();
             }
         });
+
     }
 
     //Fonction permettant de modifier une valeur donnée
-    public void enregistrerDonnee(String donnee, String valeur){
+    public void enregistrerDonnee(String donnee, String valeur) {
         String UserId = getUid();
         mDatabase.child("users").child(UserId).child(donnee).setValue(valeur);
     }
